d03099b23b3729fb115f0faa4901e3895bc47ccc
Ahmad <wisamfaithful@gmail.com>
2015-03-18 00:41:14 -0400

    added SQL to convert ips

--- db/migrate/20130416230652_convert_string_ips_to_integers.rb
+++ db/migrate/20130416230652_convert_string_ips_to_integers.rb
@@ -1,12 +1,23 @@
 class ConvertStringIpsToIntegers < ActiveRecord::Migration
 
+=begin
+ this will work in postgresql but not in sqlite
+ because in sqlite there is no alter command
   def up
-    change_column :users, :current_sign_in_ip, :integer
-    change_column :users, :last_sign_in_ip,    :integer
+    connection.execute(%q{
+      alter table users
+      alter column current_sign_in_ip type integer
+      using cast(current_sign_in_ip as integer)
+    })
   end
 
   def down
-    change_column :users, :current_sign_in_ip, :string
-    change_column :users, :last_sign_in_ip,    :string
+    connection.execute(%q{
+      alter table users
+      alter column current_sign_in_ip type varchar
+      using cast(current_sign_in_ip as varchar)
+    })
   end
+=end
 end
+
